5fd431e9b02eee5d04f2ce47020b964e7ea32e96
Renan Franca <renan.andradefranca@gmail.com>
2024-01-12 10:55:07 -0300

    Apply spring-boot module

--- src/main/java/com/mycompany/myapp/JhipsterSampleApplicationApp.java
+++ src/main/java/com/mycompany/myapp/JhipsterSampleApplicationApp.java
@@ -2,14 +2,13 @@ package com.mycompany.myapp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import tech.jhipster.lite.JHLiteApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 
 import com.mycompany.myapp.shared.generation.domain.ExcludeFromGeneratedCodeCoverage;
 
-@SpringBootApplication(scanBasePackageClasses = { JHLiteApp.class, JhipsterSampleApplicationApp.class })
+@SpringBootApplication
 @ExcludeFromGeneratedCodeCoverage(reason = "Not testing logs")
 public class JhipsterSampleApplicationApp {
 
